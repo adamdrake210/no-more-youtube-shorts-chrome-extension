e1e0bf1e2bfa82b1038eea33ce096c9394353e43
Adam Drake <adam@chemify.io>
2025-10-31 08:32:56 +0100

    making an onboarding page

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(({ reason }) => {
   if (reason === 'install') {
     chrome.tabs.create({
-      url: 'onboarding/index.html',
+      url: 'src/onboarding/index.html',
     });
   }
 });
